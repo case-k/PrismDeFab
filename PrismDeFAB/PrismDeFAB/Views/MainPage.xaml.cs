50b1e3a2eb4055688c2c575f3c2cfb1df6dca227
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2017-01-31 10:15:20 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeFAB/PrismDeFAB/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeFAB.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
